2b2192efcec172219a9dccd2eb03f864a226df38
Curro Perez Bernal <currix@gmail.com>
2014-02-19 19:08:20 +0100

    launcher working with bound states computed with octave

--- basic_definitions_1D_tise_gen_MORSE.m
+++ basic_definitions_1D_tise_gen_MORSE.m
@@ -9,7 +9,7 @@
 ##
 ##
 ## Verbosity flag
-global iprint = 0;
+global iprint = 1;
 ##
 ## graphics_toolkit("fltk")
 ##
@@ -32,7 +32,8 @@ endif
 ## Define global variables characterizing the 1D potential
 ##
 ## Morse Potential parameters 
-global we = 35.0; # (MeV)
+###global we = 35.0; # (MeV)
+global we = 33.837846; # (MeV)
 global wexe = 5.0; # (MeV)
 global R_m = 2.0; # Potential Radius (fm) (Not included in calculations, done with respect to x = R-Re
 global V_m = we^2/(4*wexe); # Potential Depth (MeV)
@@ -49,12 +50,8 @@ if ( iprint >= 1 )
   spectrum
 endif
 ##
-## Matrix diagonalization states
-global eigenvectors_file = "ho_eigenvectors_N150_Morse.dat";
-global dim_N = 150;
 global bound_states = 3;
-global pseudo_states = dim_N - bound_states;
-####
+##
 #################################################################
 #################################################### Calculations
 #################################################################
@@ -71,11 +68,11 @@ if ( iprint >= 1 )
 endif
 ##
 ## Spatial grid for bound state calculation
-global xmin = -5; # (fm)
-global xmax = 30;  # (fm)
+global xmin = -4; # (fm)
+global xmax = 35;  # (fm)
 global npoints = 1003; 
 global xgrid  = linspace(xmin,xmax,npoints); # Interval comprising ends with npoints points (fm)
-global x_step = (xmax-xmin)/(npoints-1); # (fm)
+global x_step = (xmax-xmin)/(npoints-1) # (fm)
 ## Output
 if ( iprint >= 1 )
   disp("####################################################################");
@@ -85,7 +82,7 @@ endif
 global vpot =  morse_1D(xgrid);
 ##
 ## Match Point (jeje, bound states calculation)
-global match_p = 121;
+global match_p = 170;
 ## Output
 if ( iprint >= 1 )
   printf(" match point = %d, x_M = %f fm\n", match_p, xgrid(match_p));
@@ -103,8 +100,9 @@ endif
 ## Save bound states wave function
 global iwf_bound_save = 1;
 ##
-##  Bound Eigenstates filenames wf_filename_1.dat wf_filename_2.dat ... 
+##  Bound Eigenstates filenames wf_filename.dat ... 
 global wf_filename = "wf_octave_bound_Morse";
+global en_filename = "en_octave_bound_Morse";
 ## 
 bound_states_eigensystem_Numerov_gen_pot_1D_tise
 ##
@@ -124,9 +122,9 @@ if ( iprint >= 1 )
 endif
 ##
 ## Momentum grid (fm-1)
-global k_min = 0.02; # fm-1
-global k_max = 3; # fm-1
-global n_k_points = 500;
+global k_min = 0.025; # fm-1
+global k_max = 1.5; # fm-1
+global n_k_points = 200;
 global k_values = linspace(k_min, k_max, n_k_points); ## Vector with k_values (fm-1)
 global E_values = (k_values.*hbarc).^2/(2*red_mass*amu) ## Energy values (MeV)
 ## Energy grid (MeV) (uncomment next lines and comment the  k grid lines)
@@ -136,12 +134,23 @@ global E_values = (k_values.*hbarc).^2/(2*red_mass*amu) ## Energy values (MeV)
 ##global E_values = linspace(E_min, E_max, n_E_points); ## Vector with energy values
 ##global k_values = sqrt((2*red_mass*amu).*E_values)/hbarc; ## Vector with energy values
 ##
-## Spatial grid for bound state calculation
-xmin = -30; # (fm)
-xmax = 30;  # (fm)
-npoints = 1503; 
-xgrid  = linspace(xmin,xmax,npoints); # Interval comprising ends with npoints points (fm)
-x_step = (xmax-xmin)/(npoints-1); # (fm)
+## Spatial grid for continuum state calculation
+xmin_old = xmin;
+xmax_old = xmax;
+xmin = -50; # (fm)
+xmax = 50;  # (fm)
+##
+## Extend the original grid to the left with the same step
+npoints_new = floor((xmin_old-xmin)/x_step + 1);
+xmin = xmin_old - npoints_new*x_step;
+xgrid  = [linspace(xmin,xmin_old,npoints_new + 1)(1:npoints_new) xgrid]; 
+##
+## Extend the original grid to the right with the same step
+npoints_new = floor((xmax-xmax_old)/x_step + 1);
+xmax = xmax_old + npoints_new*x_step;
+xgrid  = [xgrid linspace(xmax_old,xmax,npoints_new + 1)(2:npoints_new + 1)]; 
+npoints = size(xgrid)(2);
+##
 ## Output
 if ( iprint >= 1 )
   disp("####################################################################");
@@ -167,6 +176,18 @@ global side_wf = 1;
 continuum_states_Numerov_gen_pot_1D_tise;
 ##
 ##
+## Complete bound states
+##
+##  Bound Eigenstates filenames wf_filename.dat ... 
+global wfb_filename = "wf_octave_bound_Morse";
+global en_filename = "en_octave_bound_Morse";
+## 
+global wf_bound;
+##
+asymptotic_bound_states_gen_pot_1D_tise;
+##
+##
+##
 ## Test sum rules
 ##
 ##
@@ -188,7 +209,7 @@ wf_filename = "wf_octave_continuum_gen_Morse";
 ##
 bound_states_sum_rules_Numerov_gen_pot_1D_tise;
 ##
-##
+## 
 ##
 ## Response function dB/dE computed with continuum states
 ##
@@ -200,18 +221,28 @@ if ( iprint >= 1 )
   disp("####################################################################");
   disp(" ");
 endif
-global i_E = 1; ## 1 -> E1  :: 2 -> E2
 ##
 global isave_dBdE = 1;
-##
-## Continuum symmetrized states
-wf_filename = "wf_octave_continuum_gen_Morse";
 ##
 ## Response function filename
 global dBdE_filename = "response_function_continuum_gen_Morse";
 ##
+## Bound States
+global wfb_fortran = 0; ## If 1 read the fortran pseudostates bound wave functions (TO DO).
+wfb_filename = "wf_octave_bound_Morse"; ## Only for  wfb_fortran /= 1
+##
+## Continuum states
+wf_filename = "wf_octave_continuum_gen_Morse";
+##
+##
+global i_E = 1; ## 1 -> E1  :: 2 -> E2
 ##
 dBdE_pure_cont_gen_states_Numerov_gen_pot_1D_tise;
+##
+i_E = 2; ## 1 -> E1  :: 2 -> E2
+##
+dBdE_pure_cont_gen_states_Numerov_gen_pot_1D_tise;
+##
 ##
 ## Pseudodensity calculation
 ##
@@ -228,10 +259,39 @@ endif
 ## Save density function
 global idensity_save = 1;
 ##
-##  Continuum Eigenstates filenames 
-wf_filename = "wf_octave_continuum_gen_Morse";
 ##
 ##  Quasidensity filename 
 global qdensity_filename = "wfc_rho_gen_Morse";
+##
+## Recompute continuum states with Fortran xgrid
+xmin = -50; # (fm)
+xmax = 50;  # (fm)
+npoints = 1503; 
+xgrid  = linspace(xmin,xmax,npoints); # Interval comprising ends with npoints points (fm)
+x_step = (xmax-xmin)/(npoints-1) # (fm)
+##
+## Potential values for continuum state calculation
+vpot =  morse_1D(xgrid);
+##
+##
+## Save continuum states wave function
+iwf_cont_save = 1;
+##  Continuum Eigenstates filenames 
+wf_filename = "wf_octave_continuum_gen_Morse";
+## Save S matrix
+iSM_save = 0; 
+##  S Matrix Filename
+smat_filename = "smatrix_octave_gen_Morse.dat";
+##
+## Left (0) or right (1) incoming continuum wave functions
+side_wf = 1;
+##
+continuum_states_Numerov_gen_pot_1D_tise;
+##
+## Matrix diagonalization states
+global eigenv_file = "ho_eigenvectors_N250_Morse.dat"
+global dim_N = 250;
+global pseudostates = 20; ## Max value dim_N - bound_states;
+####
 ##
 pseudodensity_gen_states_Numerov_gen_pot_1D_tise;
