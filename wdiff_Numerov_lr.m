225dd232d4109b201c05df43400ffb0e3ef48ba5
Currix <currix@server.fake>
2014-02-22 15:57:56 +0100

    Added renormalization to equate derivatives in the matching point.

--- wdiff_Numerov_lr.m
+++ wdiff_Numerov_lr.m
@@ -16,16 +16,23 @@ function wf_difference = wdiff_Numerov_lr(e0)
   global norm_val;
   norm_val = 1.0;
   ##
+  global right_val;
+  right_val = 1.0;
+  ##
+  ##
   [wfl wfr wfpl wfpr] = w_Numerov_lr(e0);
   ##
-  ## Renormalization test
-  norm_val = abs(1/wfl);
-  wfl *= norm_val;
-  wfr *= norm_val;
-  wfpl *= norm_val;
-  wfpr *= norm_val;
   ##
-  wf_difference = wfl-wfr;
-  ## [e0,wf_difference]
+  ## Renormalization
+  norm_val = 1/wfl;
+  right_val = wfpl/wfpr;
+  ##
+  wfl *= norm_val;
+  wfpl *= norm_val;
+  wfr *= norm_val*right_val;
+  wfpr *= norm_val*right_val;
+  ##
+  wf_difference = sign(right_val)*(wfl-wfr); # To have a definite criteria with function signs
+  ##
   ##
 endfunction
